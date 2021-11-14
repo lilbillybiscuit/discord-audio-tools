c26a1efe8c509835d147ab1911d221a6f77f1d47
lilbillybiscuit <qianbill2014@outlook.com>
2021-11-14 22:53:45 +0000

    Baseline Bot

---
+++ events/ready.js
@@ -0,0 +1,4 @@
+module.exports = (client) => {
+  console.log(`Logged in to ${client.user.tag}`)
+  //console.log(`Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
+}
